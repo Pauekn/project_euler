b3639c849ecc6c23c9533d7029cc049d4ab2097d
Pauekn <pauekn@gmail.com>
2014-07-07 16:28:50 +0200

    Added problem description at the head of all current solutions

--- number_2.py
+++ number_2.py
@@ -1,3 +1,8 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 import sys
  
 def get_fibonacci(last_fibonacci):
@@ -22,4 +27,4 @@ def main():
     print(sum_even_fibonacci(last_fibonacci))
  
 if __name__ == '__main__':
-    sys.exit(main()
+    sys.exit(main())
