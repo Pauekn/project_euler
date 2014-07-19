151c071c4efd6fb6c2cf620be31b8f373db35528
Pauekn <Pauekn@Gmail.com>
2014-07-20 01:11:47 +0200

    Changed the comprehension to a generator

--- number_1.py
+++ number_1.py
@@ -13,7 +13,7 @@ def is_multiple_of_three_or_five(number):
     return number % 3 == 0 or number % 5 == 0
    
 def  sum_of_multiples(range_):
-    return sum([x for x  in range(range_) if is_multiple_of_three_or_five(x)])
+    return sum(x for x  in range(range_) if is_multiple_of_three_or_five(x))
  
 def main():
     print(sum_of_multiples(1000))    
