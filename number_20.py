ee04d964cf990f4108e1242abea5da7ab1904315
Pauekn <Pauekn@Gmail.com>
2014-07-30 13:37:41 +0200

    Cleared number 20

--- number_20.py
+++ number_20.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 n! means n × (n − 1) × ... × 3 × 2 × 1
 
@@ -7,12 +8,6 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!
 """
 import sys
-from functools import reduce
-from operator import mul
-
-
-def product(list_):
-    return reduce(mul, list_)
 
 
 def get_factorial(n):
@@ -22,8 +17,7 @@ def get_factorial(n):
     if n < 0:
         return 0  # Illegal value. Evaluates to none
 
-    # Wish to yield 1 once if n is zero. Twice otherwise
-    for i in range((n >= 1) + 1):  # Will be 2 if n >= 1. Else 1
+    if n == 1:
         yield 1
 
     while i < n+1:
@@ -32,12 +26,14 @@ def get_factorial(n):
         i += 1
 
 
-def factorial(n):
-    return product(get_factorial(n))
+def sum_factorial_digits(n):
+    last_factorial = max(get_factorial(n))
+    # A number can be looped over if converted to string. Hence the str()
+    return sum(map(int, str(last_factorial)))
+
 
 def main():
-    print(factorial(100))
+    print(sum_factorial_digits(100))
 
 if __name__ == '__main__':
     sys.exit(main())
-
